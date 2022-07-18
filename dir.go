af0daef340454e2c741edb7aaaa8c726b979d28f
lcs <lcs.shun@foxmail.com>
2022-07-18 11:30:06 +0800

    Add cmd function to read directory

--- dir.go
+++ dir.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"sort"
+)
 
 type dirCmd struct {
 	path     string
@@ -14,21 +19,47 @@ func newDirCmd(path string) cmder {
 	}
 }
 
-func (d *dirCmd) run(opts map[string]string) (code int, err error) {
-	if len(opts) == 0 {
-		// read directory messages
-
-	}
-
+func (d *dirCmd) run(opts map[string]string) (int, error) {
 	opts = d.orderOpt.extract(opts)
 	if len(opts) != 0 {
 		return ExitCodeFailed, fmt.Errorf("the count of directory does not support options: [%s]", serializeMap(opts))
 	}
-
-	return
+	d.readFileNames(d.path, "")
+	return ExitCodeSuccess, nil
 }
 
-func (d *dirCmd) readFileNames() error {
-	
-	return nil
+func (d *dirCmd) readFileNames(path, prefix string) {
+	entries, err := os.ReadDir(path)
+	if err != nil {
+		fmt.Printf("[ERROR]: failed to read file: %v\n", err)
+		os.Exit(ExitCodeFailed)
+	}
+	entries = d.sortEntries(entries)
+	prefix += "│----"
+	for _, e := range entries {
+		fmt.Println(prefix + e.Name())
+		if e.IsDir() {
+			p := filepath.Join(path, e.Name())
+			d.readFileNames(p, prefix)
+			continue
+		}
+	}
+}
+
+func (d *dirCmd) sortEntries(values []os.DirEntry) []os.DirEntry {
+	var less func(i, j int) bool
+	switch d.orderOpt.value() {
+	case orderValueDesc:
+		less = func(i, j int) bool {
+			return values[i].Name() > values[j].Name()
+		}
+	case orderValueAsc:
+		less = func(i, j int) bool {
+			return values[i].Name() < values[j].Name()
+		}
+	default:
+		return values
+	}
+	sort.SliceStable(values, less)
+	return values
 }
