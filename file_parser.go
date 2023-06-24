39309c93027756305e9f223bbc98aeda8ff656c5
lcs <lcs.shun@foxmail.com>
2023-06-24 23:20:28 +0800

    add logic of golang code parser

--- file_parser.go
+++ file_parser.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 type FileParser struct {
@@ -18,13 +20,14 @@ func NewFileParser() *FileParser {
 type parser interface {
 	Name() string
 	Match(fileName string) bool
-	Parse(file os.File) parserResult
+	Parse(file *os.File) parserResult
 }
 
 type parserResult struct {
 	CommentLines int
 	BlankLines   int
 	CodeLines    int
+	err          error
 }
 
 // parse golang file
@@ -48,7 +51,37 @@ func (p *golangParser) Match(fileName string) bool {
 	return filepath.Ext(fileName) == p.ext
 }
 
-func (p *golangParser) Parse(file os.File) parserResult {
-	// TODO: add codes
+func (p *golangParser) Parse(file *os.File) parserResult {
+	scaner := bufio.NewScanner(file)
+	var comments, blanks, codes int
+	var isLongCommnets bool
+	var isInLongSentence bool
+	for scaner.Scan() {
+		text := scaner.Text()
+		// if the line is blank
+		if text == "" {
+			blanks++
+			continue
+		}
+		var isInQuoteSentence bool
+		if index := strings.Index(text, "/*"); index != -1 {
+			for i := index - 1; i >= 0; i-- {
+				if text[i] != '"' {
+					continue
+				}
+				if index-1 >= 0 && text[index-1] != '\\' {
+					isInQuoteSentence = true
+				}
+			}
+		}
+
+		if strings.Contains(text, "//") {
+			// 判断该
+		}
+
+	}
+	if err := scaner.Err(); err != nil {
+		return parserResult{err: err}
+	}
 	return parserResult{}
 }
